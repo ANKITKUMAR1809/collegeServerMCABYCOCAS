63649b0b220c806258dfb9581bb9b30a330400e8
ANKITKUMAR1809 <skwedsak143@gmail.com>
2025-05-22 02:32:58 +0530

    done with the changes

--- routers/studentRoute.js
+++ routers/studentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const studentRouter = express.Router();
-const studentController = require('../controllers/studentController.js');
+const studentController = require('../controllers/studentcontroller.js');
 
 studentRouter.post("/register-student",studentController.postRegisterStudents)
 studentRouter.get("/register-student",studentController.getRegisterStudents)
