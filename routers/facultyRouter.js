d5580f901e71e200ec9b79f0bae585e1ff4f6eba
ANKITKUMAR1809 <skwedsak143@gmail.com>
2025-05-22 23:36:30 +0530

    route error solved

--- routers/facultyRouter.js
+++ routers/facultyRouter.js
@@ -11,9 +11,9 @@ facultyRouter.post("/faculty-signup", facultyController.facultySignup);
 facultyRouter.post("/faculty-login", facultyController.facultyLogin);
 facultyRouter.get("/faculty-session", facultyController.facultySession);
 facultyRouter.get("/faculty-logout", facultyController.facultyLogout);
-facultyRouter.post("faculty/forget-password", facultyController.facultyForgetPassword);
-facultyRouter.post("faculty/reset-password/:email", facultyController.facultyResetPassword);
-facultyRouter.post("faculty/verify-otp/:email", facultyController.facultyVerifyOtp);
+facultyRouter.post("/faculty/forget-password", facultyController.facultyForgetPassword);
+facultyRouter.post("/faculty/reset-password/:email", facultyController.facultyResetPassword);
+facultyRouter.post("/faculty/verify-otp/:email", facultyController.facultyVerifyOtp);
 
 
 module.exports = facultyRouter;
